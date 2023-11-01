78742785c39b2ba4158f9c628ea9f2e786425418
Yash693218 <145857513+Yash693218@users.noreply.github.com>
2023-11-01 09:08:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Web Designer', 'Video Editor', 'Freelancer' , 'Graphic Designer' , 'And a part-time Gamer.'],
+    typeSpeed: 50,
+  });
